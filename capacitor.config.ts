9ecb9faa781373449622ed5f2228d89818050941
ThomasJuty <thomasjuti1210@gmail.com>
2026-03-07 18:44:26 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FastPizza ',
+  webDir: 'www'
+};
+
+export default config;
